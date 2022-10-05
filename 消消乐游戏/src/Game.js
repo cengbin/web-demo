f342723b6e546fe3fa5be4bec8101efcb004aed0
bin.zeng <yy282371715@sina.cn>
2022-10-05 19:24:33 +0800

    update

--- 消消乐游戏/src/Game.js
+++ 消消乐游戏/src/Game.js
@@ -1,5 +1,4 @@
-// import Map from "./Map.js"
-import ApplicationFacade from "./org/ApplicationFacade.js"
+import Map from './Map.js'
 
 export default class Game extends PIXI.Application {
 
@@ -8,10 +7,7 @@ export default class Game extends PIXI.Application {
 
     document.body.appendChild(this.view)
 
-    // let map = new Map()
-    // this.stage.addChild(map)
-
-    // let app = ApplicationFacade.getInstance(Game.NAME) as ApplicationFacade
-    // app.startup(this)
+    let map = new Map()
+    this.stage.addChild(map)
   }
 }
